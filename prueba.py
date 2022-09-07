1954a04c3c304fa9c55a52cae90da663dd6855bf
GerardoMH <geramalakian@gmail.com>
2022-09-07 00:54:05 -0500

    RNA Básica. Minibatch=15 Capa interm extra de 50 Neuronas. Incremento de 1.2% en los aciertos respecto a la original

---
+++ prueba.py
@@ -0,0 +1,28 @@
+import mnist_loader
+import network
+import pickle
+
+
+training_data, validation_data , test_data = mnist_loadger.load_data_wrapper()
+
+training_data = list(training_data)
+test_data = list(test_data)
+
+net=network.Network([784,50,30,10])
+
+net.SGD( training_data, 30, 15, 3, test_data=test_data)
+
+archivo = open("red_prueba1.pkl",'wb')
+pickle.dump(net,archivo)
+archivo.close()
+exit()
+#leer el archivo
+
+archivo_lectura = open("red_prueba.pkl",'rb')
+net = pickle.load(archivo_lectura)
+archivo_lectura.close()
+net.SGD( training_data, 10, 50, 0.5, test_data=test_data)
+archivo = open("red_prueba.pkl",'wb')
+pickle.dump(net,archivo)
+archivo.close()
+exit()
